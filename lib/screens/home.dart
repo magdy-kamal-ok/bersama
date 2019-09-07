2f8d2af9448d85a35b6a18a9e544a07fb5fc585a
Alex Edward Ball <ballalexedward@gmail.com>
2019-09-06 21:30:43 -0700

    have background image working

---
+++ lib/screens/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background_home.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: null /* add child content here */,
+      ),
+    );
+  }
+}
