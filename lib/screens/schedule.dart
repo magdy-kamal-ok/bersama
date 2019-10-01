492c330778ae5cdeb1c58b559bab3f8b727c8c4c
Alex Edward Ball <ballalexedward@gmail.com>
2019-09-30 20:40:14 -0700

    added horizontal list

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -12,10 +12,45 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Bottom Bar'),
+      appBar: AppBar(
+        title: Text('Schedule'),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 200.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Container(
+              width: 160.0,
+              color: Colors.red,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.green,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.orange,
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomBar(onTap: null, currentIndex: 1),
     );
   }
 }
+
+// title: title,
+//       home: Scaffold(
+
+//       ),
+//     );
