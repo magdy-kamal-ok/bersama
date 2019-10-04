494468517ca96dbad680aacd31a35f5bba4e6955
Alex Edward Ball <ballalexedward@gmail.com>
2019-10-04 12:05:05 -0700

    scaffold of video page

--- lib/screens/video.dart
+++ lib/screens/video.dart
@@ -13,7 +13,37 @@ class _VideoScreenState extends State<VideoScreen> {
   VideoPlayerController _controller;
 
   @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.asset('assets/yoga.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+    _controller.play();
+    _controller.setLooping(true);
+    _controller.setVolume(1.0);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              child: _controller.value.initialized
+                  ? AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    )
+                  : Container(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
